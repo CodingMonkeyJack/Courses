3bca4a6abdc432d76b4ebfa56d199af0d65a0f21
Lilong <jianglilong0225@gmaill.com>
2016-04-13 23:05:50 -0400

    upload linear regression

--- ML/src/MLProject/WebContent/static/regression.js
+++ ML/src/MLProject/WebContent/static/regression.js
@@ -1,4 +1,7 @@
 // demo: http://www.shodor.org/interactivate/activities/Regression/
+/*
+ * TODO: calculate residuals
+ * */
 function loadRegressionControls() {
 	var paramHolder = $("div#params");
 	paramHolder.children().remove();
@@ -28,9 +31,9 @@ function regressionFit() {
 	var result = regression('linear', normData);
 	var m = result['equation'][0], b = result['equation'][1];
 	var startY = m * startX + b, endY = m * endX + b;
-	console.log('m:' + m + ' b:' + b);
-	console.log(startX + ' ' + endX);
-	console.log(startY + ' ' + endY);
+//	console.log('m:' + m + ' b:' + b);
+//	console.log(startX + ' ' + endX);
+//	console.log(startY + ' ' + endY);
 	var startPosX = x(startX) + margin.left, startPosY = y(startY) + margin.top,
 		endPosX = x(endX) + margin.left, endPosY = y(endY) + margin.top;
 	
@@ -51,10 +54,25 @@ function regressionFit() {
     .style({'fill': 'red', 'opacity': 0.6});
 	
 	svg.append("line")
+	.attr("id", "regline")
 	.attr("x1", startPosX)
 	.attr("y1", startPosY)
 	.attr("x2", endPosX)
 	.attr("y2", endPosY) //stroke="gray" ="5"  
 	.attr("stroke", "gray")
 	.attr("stroke-width", 5);
+	
+	$('#regline')
+	  .draggable();
+	  .bind('mousedown', function(event, ui){
+	    // bring target to front
+		  console.log('mouse down');
+		  $(event.target.parentElement).append( event.target );
+	  })
+	  .bind('drag', function(event, ui){
+	    // update coordinates manually, since top/left style props don't work on SVG
+		  console.log('drag');
+		  event.target.setAttribute('x', ui.position.left);
+		  event.target.setAttribute('y', ui.position.top);
+	  });
 }
